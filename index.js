4ff5e8a6e4e0ab04e95b68103e133165dee33015
Javier Gascon <javitocatral@gmail.com>
2024-08-28 15:59:28 +0200

    primer lab del dia 3

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
 /*******************************************
     Iteration 1.1 | Tongue Twister
 *******************************************/
-const s1 = "Fred";
-const s2 = "fed";
-const s3 = "Ted";
-const s4 = "bread";
-const s5 = "and";
+const s1 = 'Fred';
+const s2 = 'fed';
+const s3 = 'Ted';
+const s4 = 'bread';
+const s5 = 'and';
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 // Print out the concatenated string
 
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
-const part1 = "java";
-const part2 = "script";
+let part1 = 'java';
+let part2 = 'script';
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+part1 =
+  part1.slice(0, part1.length - 1) + part1[part1.length - 1].toUpperCase();
+part2 =
+  part2.slice(0, part2.length - 1) + part2[part2.length - 1].toUpperCase();
 
+let result = part1 + part2;
 
 // Print the cameLtaiL-formatted string
-
-
-
-
+console.log(result);
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
-
+let tipAmount = 84 * (15 / 100);
 // Print out the tipAmount
-
-
-
-
+console.log(tipAmount);
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+let numAleatorio = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
-
-
-
+console.log(numAleatorio);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -62,15 +57,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+b;
 
 const expression2 = a || b;
+a;
 
 const expression3 = !a && b;
+b;
 
 const expression4 = !(a && b);
+a;
 
 const expression5 = !a || !b;
+a;
 
 const expression6 = !(a || b);
+b;
 
 const expression7 = a && a;
+a;
